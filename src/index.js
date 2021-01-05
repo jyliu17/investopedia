62abeb958937fdddae335355dd5006fda5f7bfa4
dashdavid13 <davidamedina619@gmail.com>
2021-01-05 17:43:55 -0500

    working on event

--- src/index.js
+++ src/index.js
@@ -1,23 +1,105 @@
-//DOM elements
-const projectList = document.querySelector("#project-list")
+/************ GLOBAL DOM elements *************/
+const listings = document.querySelector("#project-collection")
+const dogModal = document.querySelector(".modal")
 
-//render 
 
-function renderProject(projObj) {
-    const projectDiv = document.createElement("div")
-    const projectImg = document.createElement("img")
-    projectImg.className = "proj-img"
-    projectImg.src = project.image
-    cont
+
+
+
+
+function renderOneProject(projectArray) {
+  /****************** DOM Elements ******************/
+
+  
+  const projectDiv = document.createElement("div");
+  const newH2 = document.createElement("h2");
+  const img = document.createElement("img");
+
+  /****************** DOM attributes ******************/
+  projectDiv.setAttribute('class', 'card');
+  img.setAttribute('class', 'project-image');
+  // newH2.textContent = projectArray.name;
+  img.src = projectArray.image;
+  
+  projectDiv.append(newH2, img);
+  listings.append(projectDiv);
+
+  img.addEventListener("click", function(event){
+    showProject()
+    console.log(event.target)
+  })
+  
+function showProject(newProject) {
+   debugger
+  fetch(`http://localhost:3000/projects/${id}`)
+  .then( r => r.json())
+  .then( newProject => {
+      let modalContent = document.querySelector("#project-modal")
+
+    
+      let child = modalContent.lastElementChild;
+      while (child) {
+          modalContent.removeChild(child);
+          child = modalContent.lastElementChild;
+      }
+      
+      let modalImg = document.createElement("img")
+      modalImg.setAttribute("class", "modal-img")
+      const p = document.createElement("p");
+      const H2 = document.createElement("h2");
+
+      modalImg.src = newProject.image_url
+      p.textContent = newProject.description
+      H2.textContent = newProject.nam
+
+
+      modalContent.append(modalImg, p, h2)
+
+      dogModal.style.display = "block";
+      scrollButton.style.display = "none";
+
+  })
 
 }
 
 
-//fetch
-function getProjects() {
-    fetch("")
-     .then(r => r.json())
-     .then(data => {
-       console.log(data)
-     })
-   }
+
+
+
+}
+
+  /******************* Fetch Array  ******************/
+  function getProjects() {
+    return fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+  }
+  
+  getProjects().then(projects => {
+    projects.forEach(project => {
+    renderOneProject(project)
+    })
+  })
+
+
+
+//   function projectShow() {
+//       return fetch(`http://localhost:3000/projects/${id}`)
+//   const projectDiv = document.createElement("div");
+//   const newH2 = document.createElement("h2");
+//   const img = document.createElement("img");
+
+
+//   /****************** DOM attributes ******************/
+//   projectDiv.setAttribute('class', 'card');
+//   img.setAttribute('class', 'project-image');
+//   // newH2.textContent = projectArray.name;
+//   img.src = projectArray.image;
+  
+//   projectDiv.append(newH2, img);
+//   listings.append(projectDiv);
+//   }
+
+
+
+
+
